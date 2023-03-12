1f797b43e268fba1cc263cf83a392031fb0999fc
akashosc <84351117+akashosc@users.noreply.github.com>
2023-03-13 02:19:30 +0530

    changed btn

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const BASE_URL="https://api.themoviedb.org/3";
 const API_URL=BASE_URL+'/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL="https://image.tmdb.org/t/p/w500";
 const searchUrl=BASE_URL+'/search/movie?'+API_KEY;
+const url_forkid=BASE_URL+'/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&'+API_KEY;
+
+
+
+
+
+
+
+
 getMovies(API_URL);
 const main=document.getElementById('main');
 const form=document.getElementById('form');
